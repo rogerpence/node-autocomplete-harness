be8035edd5f37c92affcc9c1f96a9b93cb3b86b3
Roger Pence <roger.pence@gmail.com>
2018-03-28 23:57:19 -0500

    add ejs and a few pages

--- server.js
+++ server.js
@@ -5,11 +5,27 @@ var path = require('path');
 const app = express();
 const port = 8080;
 
+app.set('view engine', 'ejs');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 require('./app/routes')(app, {});
 
 app.get('/', function(req, res) {
+
+  var drinks = [
+    { name: 'Bloody Mary', drunkness: 3 },
+    { name: 'Martini', drunkness: 5 },
+    { name: 'Scotch', drunkness: 10 }
+  ];  
+
+  res.render('pages/index', {
+    drinks: drinks
+  });
+});
+
+
+app.get('/single', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'))
 });
 
@@ -20,6 +36,7 @@ app.get('/cascade', function(req, res) {
 app.get('/multiple', function(req, res) {
   res.sendFile(path.join(__dirname + '/multiple.html'))
 });
+
 
 
 app.listen(port, () => {
